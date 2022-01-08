3c5bdbc68a4542f5980c589f5f348e2e27d0f6a1
alpha-ome-ga <85073134+alpha-ome-ga@users.noreply.github.com>
2022-01-09 02:11:08 +0530

    Update code.js

--- code.js
+++ code.js
@@ -1,26 +1,158 @@
-window.onload = choosePic;
+window.onload = nmsChoose;
 
-var myImgsA = new Array(
-  {imgLink:"https://images.pexels.com/photos/169573/pexels-photo-169573.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", 
-  affLink:"https://www.gmail.com"}, 
-  {imgLink:"https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", 
-  affLink:"https://www.google.com"},
-  {imgLink:"https://images.pexels.com/photos/270348/pexels-photo-270348.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", 
-  affLink:"https://www.youtube.com"})
+  var common30x25 = new Array(
+  {imgLink:"https://www.ledgerwallet.com/affiliate/image/250/100", affLink:"https://bit.ly/32IYH36"},
+  {imgLink:"https://central.ck-cdn.com/fiverr/2021-04-08/General-300x250-A_ee088cbb.jpg", affLink:"https://bit.ly/3pUYSAX"},
+  {imgLink:"https://central.ck-cdn.com/fiverr/2021-04-08/General-300x250-A-1_588aaf4a.jpg", affLink:"https://bit.ly/3pUYSAX"},
+  {imgLink:"https://central.ck-cdn.com/fiverr/2021-03-30/1029General300x250A_bb1b507f.jpg", affLink:"https://bit.ly/3FWSkYj"},
+  {imgLink:"https://central.ck-cdn.com/fiverr/2021-03-30/5734General300x250A_c8804f01.jpg", affLink:"https://bit.ly/3FWSkYj"},
+  {imgLink:"https://central.ck-cdn.com/fiverr/2021-03-30/3842General300x250A1_b9b491e0.jpg", affLink:"https://bit.ly/3FWSkYj"},
+  {imgLink:"https://bluehost-cdn.com/media/partner/images/cashomega/300x250/300x250BW.png", affLink:"https://bit.ly/3gW7ocf"})
+  
+  
+  var commonFooter = new Array(
+  {imgLink:"https://www.ledgerwallet.com/affiliate/image/728/90", affLink:"https://bit.ly/32IYH36"},
+  {imgLink:"https://central.ck-cdn.com/fiverr/2021-04-08/General-728x90-B_51d1d2fd.jpg", affLink:"https://bit.ly/3pUYSAX"},
+  {imgLink:"https://central.ck-cdn.com/fiverr/2021-04-08/General-728x90-B-1_2a5804d8.jpg", affLink:"https://bit.ly/3pUYSAX"},
+  {imgLink:"https://central.ck-cdn.com/fiverr/2021-03-30/1029General728x90B_eda81ffe.jpg", affLink:"https://bit.ly/3FWSkYj"},
+  {imgLink:"https://central.ck-cdn.com/fiverr/2021-03-30/5734General728x90B_d8ddd580.jpg", affLink:"https://bit.ly/3FWSkYj"},
+  {imgLink:"https://central.ck-cdn.com/fiverr/2021-03-30/3842General728x90B1_0aa3e592.jpg", affLink:"https://bit.ly/3FWSkYj"},
+  {imgLink:"https://bluehost-cdn.com/media/partner/images/cashomega/728x90/728x90BW.png", affLink:"https://bit.ly/3gW7ocf"},
+  {imgLink:"https://bluehost-cdn.com/media/partner/images/cashomega/760x80/760x80BW.png", affLink:"https://bit.ly/3gW7ocf"})
+  
+  var commonSolos = new Array(
+  {imgLink:"https://central.ck-cdn.com/fiverr/2021-04-08/General_16X9_7894bae7.jpg", affLink:"https://bit.ly/3pUYSAX"},
+  {imgLink:"https://central.ck-cdn.com/fiverr/2021-04-08/General_16X9-1_c606993b.jpg", affLink:"https://bit.ly/3pUYSAX"},
+  {imgLink:"https://central.ck-cdn.com/fiverr/2021-03-30/5734General16X9_427703e6.jpg", affLink:"https://bit.ly/3FWSkYj"},
+  {imgLink:"https://central.ck-cdn.com/fiverr/2021-03-30/3842General16X91_fae23d3a.jpg", affLink:"https://bit.ly/3FWSkYj"},
+  {imgLink:"https://bluehost-cdn.com/media/partner/images/cashomega/430x288/430x288BW.png", affLink:"https://bit.ly/3gW7ocf"},
+  {imgLink:"https://bluehost-cdn.com/media/partner/images/cashomega/430x288/430x288PW.png", affLink:"https://bit.ly/3gW7ocf"},
+  {imgLink:"https://bluehost-cdn.com/media/partner/images/cashomega/488x160/488x160BW.png", affLink:"https://bit.ly/3gW7ocf"},
+  {imgLink:"https://bluehost-cdn.com/media/partner/images/cashomega/620x203/620x203BW.png", affLink:"https://bit.ly/3gW7ocf"},
+  {imgLink:"https://i.ibb.co/ThQdjqW/H-LEDDevice-EMAILGraphics-150dpi-01.jpg", affLink:"https://bit.ly/3FYqKK8"},
+  {imgLink:"https://i.ibb.co/86BgJVC/H-LEDDevice-EMAILGraphics-150dpi-02.jpg", affLink:"https://bit.ly/3FYqKK8"},
+  {imgLink:"https://i.ibb.co/dMWpTty/PLOLifestyle-4-Slider.jpg", affLink:"https://bit.ly/3FYqKK8"},
+  {imgLink:"https://i.ibb.co/RQjwFsP/PLOLifestyle-5-Slider.jpg", affLink:"https://bit.ly/3FYqKK8"},
+  {imgLink:"https://i.ibb.co/8bJJLkb/PLOLifestyle-8-Slider.jpg", affLink:"https://bit.ly/3FYqKK8"},
+  {imgLink:"https://i.ibb.co/7VVTqjQ/PLOLifestyle-9-Slider.jpg", affLink:"https://bit.ly/3FYqKK8"})
+  
+  var nms30x25 = new Array(
+    {imgLink:"https://callofdestiny.org/affiliate/uploads/5.jpg", affLink:"https://bit.ly/3wTFTYc"},
+    {imgLink:"https://callofdestiny.org/affiliate/uploads/5a.jpg", affLink:"https://bit.ly/3wTFTYc"},
+    {imgLink:"https://i.ibb.co/JvK0mjs/Banner3-300x250.jpg", affLink:"https://bit.ly/3wTFTYc"},
+    {imgLink:"https://i.ibb.co/tmkSgpF/Banner4-300x250.jpg", affLink:"https://bit.ly/3wTFTYc"},
+    {imgLink:"https://i.ibb.co/R0b6NGQ/Banner13-300x250.jpg", affLink:"https://bit.ly/3wTFTYc"},
+    {imgLink:"https://i.ibb.co/dpkYWRj/New-Astrology-Banner01-300x250.jpg", affLink:"https://bit.ly/3wTFTYc"},
+    {imgLink:"https://i.ibb.co/7gYpgRB/New-Astrology-Banner06-300x250.jpg", affLink:"https://bit.ly/3wTFTYc"},
+    {imgLink:"https://i.ibb.co/DYvWQGn/New-Astrology-Banner07-300x250.jpg", affLink:"https://bit.ly/3wTFTYc"},
+    {imgLink:"https://i.ibb.co/LC9S2y9/New-Astrology-Banner08-300x250.jpg", affLink:"https://bit.ly/3wTFTYc"},
+    {imgLink:"https://www.individualogist.com/affiliate/assets/img/banner-300x250.png", affLink:"https://bit.ly/3HSoFjB"})
+    
+    var nms25x25 = new Array(
+    {imgLink:"https://callofdestiny.org/affiliate/uploads/4.jpg", affLink:"https://bit.ly/3wTFTYc"},
+    {imgLink:"https://callofdestiny.org/affiliate/uploads/4a.jpg", affLink:"https://bit.ly/3wTFTYc"},
+    {imgLink:"https://i.ibb.co/S0mNd53/Banner3-250x250.jpg", affLink:"https://bit.ly/3wTFTYc"},
+    {imgLink:"https://i.ibb.co/bNZn1FL/Banner4-250x250.jpg", affLink:"https://bit.ly/3wTFTYc"},
+    {imgLink:"https://i.ibb.co/cNdXcBG/Banner13-250x250.jpg", affLink:"https://bit.ly/3wTFTYc"},
+    {imgLink:"https://i.ibb.co/M1NxJGk/New-Astrology-Banner01-250x250.jpg", affLink:"https://bit.ly/3wTFTYc"},
+    {imgLink:"https://i.ibb.co/2SfB0pB/New-Astrology-Banner06-250x250.jpg", affLink:"https://bit.ly/3wTFTYc"},
+    {imgLink:"https://i.ibb.co/sFWtbzP/New-Astrology-Banner07-250x250.jpg", affLink:"https://bit.ly/3wTFTYc"},
+    {imgLink:"https://i.ibb.co/h7278kk/New-Astrology-Banner08-250x250.jpg", affLink:"https://bit.ly/3wTFTYc"},
+    {imgLink:"https://www.individualogist.com/affiliate/assets/img/banner-250x250.png", affLink:"https://bit.ly/3HSoFjB"})
+    
+    var nmssolos = new Array(
+    {imgLink:"https://i.ibb.co/tL4d5gM/Banner3-336x280.jpg", affLink:"https://bit.ly/3wTFTYc"},
+    {imgLink:"https://i.ibb.co/nnmm1g2/Banner4-336x280.jpg", affLink:"https://bit.ly/3wTFTYc"},
+    {imgLink:"https://i.ibb.co/qYxFCbP/Banner13-336x280.jpg", affLink:"https://bit.ly/3wTFTYc"},
+    {imgLink:"https://i.ibb.co/YjCDKtL/New-Astrology-Banner01-336x280.jpg", affLink:"https://bit.ly/3wTFTYc"},
+    {imgLink:"https://i.ibb.co/6v91Kkh/New-Astrology-Banner06-336x280.jpg", affLink:"https://bit.ly/3wTFTYc"},
+    {imgLink:"https://i.ibb.co/r30yShj/New-Astrology-Banner07-336x280.jpg", affLink:"https://bit.ly/3wTFTYc"},
+    {imgLink:"https://i.ibb.co/W3RtGzr/New-Astrology-Banner08-336x280.jpg", affLink:"https://bit.ly/3wTFTYc"},
+    {imgLink:"https://callofdestiny.org/affiliate/uploads/9.jpg", affLink:"https://bit.ly/3wTFTYc"},
+    {imgLink:"https://callofdestiny.org/affiliate/uploads/9a.jpg", affLink:"https://bit.ly/3wTFTYc"})
+    
+    var nmsfooter = new Array(
+    {imgLink:"https://i.ibb.co/bm5vbKD/1200x628.jpg", affLink:"https://bit.ly/3wTFTYc"},
+    {imgLink:"https://i.ibb.co/g3S66gt/970x250.jpg", affLink:"https://bit.ly/3wTFTYc"})
+    
 
-var myImgsB = new Array(
-  {imgLink:"https://images.pexels.com/photos/169573/pexels-photo-169573.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", 
-  affLink:"https://www.gmail.com"}, 
-  {imgLink:"https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", 
-  affLink:"https://www.google.com"},
-  {imgLink:"https://images.pexels.com/photos/270348/pexels-photo-270348.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", 
-  affLink:"https://www.youtube.com"})  
-
-function choosePic() {
-     var randomNum = Math.floor(Math.random() * myImgsA.length);
-     document.getElementById("myPicture1").src = myImgsA[randomNum].imgLink;
-     document.getElementById("myLink1").href = myImgsA[randomNum].affLink;
-     var randomNum = Math.floor(Math.random() * myImgsB.length);
-     document.getElementById("myPicture2").src = myImgsB[randomNum].imgLink;
-     document.getElementById("myLink2").href = myImgsB[randomNum].affLink;
+function nmsChoose() {
+  var randomNum = Math.floor(Math.random() * common30x25.length);
+  document.getElementById("325ca1").src = common30x25[randomNum].imgLink;
+  document.getElementById("325ca1l").href = common30x25[randomNum].affLink;
+  var randomNum = Math.floor(Math.random() * common30x25.length);
+  document.getElementById("325ca2").src = common30x25[randomNum].imgLink;
+  document.getElementById("325ca2l").href = common30x25[randomNum].affLink;
+  var randomNum = Math.floor(Math.random() * common30x25.length);
+  document.getElementById("325cb1").src = common30x25[randomNum].imgLink;
+  document.getElementById("325cb1l").href = common30x25[randomNum].affLink;
+  var randomNum = Math.floor(Math.random() * common30x25.length);
+  document.getElementById("325cb2").src = common30x25[randomNum].imgLink;
+  document.getElementById("325cb2l").href = common30x25[randomNum].affLink;
+  var randomNum = Math.floor(Math.random() * common30x25.length);
+  document.getElementById("325cc1").src = common30x25[randomNum].imgLink;
+  document.getElementById("325cc1l").href = common30x25[randomNum].affLink;
+  var randomNum = Math.floor(Math.random() * common30x25.length);
+  document.getElementById("325cc2").src = common30x25[randomNum].imgLink;
+  document.getElementById("325cc2l").href = common30x25[randomNum].affLink;
+  var randomNum = Math.floor(Math.random() * commonFooter.length);
+  document.getElementById("fca").src = commonFooter[randomNum].imgLink;
+  document.getElementById("fcal").href = commonFooter[randomNum].affLink;
+  var randomNum = Math.floor(Math.random() * commonSolos.length);
+  document.getElementById("sca").src = commonSolos[randomNum].imgLink;
+  document.getElementById("scal").href = commonSolos[randomNum].affLink;
+  var randomNum = Math.floor(Math.random() * commonSolos.length);
+  document.getElementById("scb").src = commonSolos[randomNum].imgLink;
+  document.getElementById("scbl").href = commonSolos[randomNum].affLink;
+  var randomNum = Math.floor(Math.random() * commonSolos.length);
+  document.getElementById("scc").src = commonSolos[randomNum].imgLink;
+  document.getElementById("sccl").href = commonSolos[randomNum].affLink;
+  var randomNum = Math.floor(Math.random() * nms30x25.length);
+  document.getElementById("325na1").src = nms30x25[randomNum].imgLink;
+  document.getElementById("325na1l").href = nms30x25[randomNum].affLink;
+  var randomNum = Math.floor(Math.random() * nms30x25.length);
+  document.getElementById("325na2").src = nms30x25[randomNum].imgLink;
+  document.getElementById("325na2l").href = nms30x25[randomNum].affLink;
+  var randomNum = Math.floor(Math.random() * nms30x25.length);
+  document.getElementById("325nb1").src = nms30x25[randomNum].imgLink;
+  document.getElementById("325nb1l").href = nms30x25[randomNum].affLink;
+  var randomNum = Math.floor(Math.random() * nms30x25.length);
+  document.getElementById("325nb2").src = nms30x25[randomNum].imgLink;
+  document.getElementById("325nb2l").href = nms30x25[randomNum].affLink;
+  var randomNum = Math.floor(Math.random() * nms30x25.length);
+  document.getElementById("325nc1").src = nms30x25[randomNum].imgLink;
+  document.getElementById("325nc1l").href = nms30x25[randomNum].affLink;
+  var randomNum = Math.floor(Math.random() * nms30x25.length);
+  document.getElementById("325nc2").src = nms30x25[randomNum].imgLink;
+  document.getElementById("325nc2l").href = nms30x25[randomNum].affLink;
+  var randomNum = Math.floor(Math.random() * nms25x25.length);
+  document.getElementById("25na1").src = nms25x25[randomNum].imgLink;
+  document.getElementById("25na1l").href = nms25x25[randomNum].affLink;
+  var randomNum = Math.floor(Math.random() * nms25x25.length);
+  document.getElementById("25na2").src = nms25x25[randomNum].imgLink;
+  document.getElementById("25na2l").href = nms25x25[randomNum].affLink;
+  var randomNum = Math.floor(Math.random() * nms25x25.length);
+  document.getElementById("25nb1").src = nms25x25[randomNum].imgLink;
+  document.getElementById("25nb1l").href = nms25x25[randomNum].affLink;
+  var randomNum = Math.floor(Math.random() * nms25x25.length);
+  document.getElementById("25nb2").src = nms25x25[randomNum].imgLink;
+  document.getElementById("25nb2l").href = nms25x25[randomNum].affLink;
+  var randomNum = Math.floor(Math.random() * nms25x25.length);
+  document.getElementById("25nc1").src = nms25x25[randomNum].imgLink;
+  document.getElementById("25nc1l").href = nms25x25[randomNum].affLink;
+  var randomNum = Math.floor(Math.random() * nms25x25.length);
+  document.getElementById("25nc2").src = nms25x25[randomNum].imgLink;
+  document.getElementById("25nc2l").href = nms25x25[randomNum].affLink;
+  var randomNum = Math.floor(Math.random() * nmssolos.length);
+  document.getElementById("sna").src = nmssolos[randomNum].imgLink;
+  document.getElementById("snal").href = nmssolos[randomNum].affLink;
+  var randomNum = Math.floor(Math.random() * nmssolos.length);
+  document.getElementById("snb").src = nmssolos[randomNum].imgLink;
+  document.getElementById("snbl").href = nmssolos[randomNum].affLink;
+  var randomNum = Math.floor(Math.random() * nmssolos.length);
+  document.getElementById("snc").src = nmssolos[randomNum].imgLink;
+  document.getElementById("sncl").href = nmssolos[randomNum].affLink;
+  var randomNum = Math.floor(Math.random() * nmsfooter.length);
+  document.getElementById("fna").src = nmsfooter[randomNum].imgLink;
+  document.getElementById("fnal").href = nmsfooter[randomNum].affLink;  
 }
